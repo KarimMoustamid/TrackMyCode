370e9c31077fd08017063058a4b7d037ce0fe064
karimmoustamid <moustamid.teck@gmail.com>
2025-02-26 06:43:40 -0600

    Add Microsoft.AspNetCore.OpenApi package to the project
    Integrated Microsoft.AspNetCore.OpenApi version 9.0.2 and related files into the project to enable OpenAPI support. Updated relevant configuration files and program setup accordingly. This change enhances API documentation and OpenAPI compliance.

--- TrackMyCode.API/obj/Debug/net9.0/TrackMyCode.API.AssemblyInfo.cs
+++ TrackMyCode.API/obj/Debug/net9.0/TrackMyCode.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackMyCode.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e2a052f69af811a2f3c38b38f893a10ea51ce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b38be08a766e3bdc83c231e693646b63fd564c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackMyCode.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackMyCode.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
