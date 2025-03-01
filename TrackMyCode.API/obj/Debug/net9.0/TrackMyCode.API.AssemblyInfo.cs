5c8ac89605aff57bb8b01317d10880eb1d61c9b1
karimmoustamid <moustamid.teck@gmail.com>
2025-03-01 15:50:37 -0600

    Update logging level and update assembly version metadata
    Adjusted the logging level for Microsoft.AspNetCore to "Information" in the development appsettings file. Updated metadata in assembly info and source link to reflect the new build version and commit reference.

--- TrackMyCode.API/obj/Debug/net9.0/TrackMyCode.API.AssemblyInfo.cs
+++ TrackMyCode.API/obj/Debug/net9.0/TrackMyCode.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackMyCode.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9ab288b86b59c179769c7c4ce9766a966def5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6e5c1686ce3778b66cbed136d81c10e48042e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackMyCode.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackMyCode.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
